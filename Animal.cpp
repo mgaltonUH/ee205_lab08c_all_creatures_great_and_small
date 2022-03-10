c6bc4114a51421400847568992e53c5e57b89f95
mgaltonUH <mgalton@hawaii.edu>
2022-03-10 10:54:37 -1000

    Finished stubbing out all of the code.

--- Animal.cpp
+++ Animal.cpp
@@ -5,6 +5,7 @@
 /// @file Animal.cpp
 /// @version 1.0
 ///
+
 /// @author Mariko Galton <mgalton@hawaii.edu>
 /// @date   10_Mar_2022
 ///////////////////////////////////////////////////////////////////////////////
@@ -12,4 +13,51 @@
 #include "Animal.h"
 
 using namespace std;
+
+
 const string Animal:: kingdom = "Animalia";
+
+Animal::Animal(const string &newspecies) : species(newspecies) {
+    Animal::species = newspecies;
+}
+
+Animal::Animal(const string &species, Gender gender) : species(species), gender(gender) {}
+
+
+
+Animal::Animal(const string &species, Gender gender, float weight) : species(species), gender(gender), weight(weight) {}
+
+Animal::Animal(const string &species, float weight) : species(species), weight(weight) {}
+
+const string &Animal::getKingdom() {
+    return kingdom;
+}
+
+const string &Animal::getSpecies() const {
+    return species;
+}
+
+Gender Animal::getGender() const {
+    return gender;
+}
+
+void Animal::setGender(Gender gender) {
+    Animal::gender = gender;
+}
+
+float Animal::getWeight() const {
+    return weight;
+}
+
+void Animal::setWeight(float weight) {
+    Animal::weight = weight;
+}
+
+void Animal::printInfo() {
+
+}
+
+bool Animal::isvalid() {
+    return false;
+}
+
--- Animal.h
+++ Animal.h
@@ -22,14 +22,41 @@ enum Gender {
 const float UNKNOWN_WEIGHT = -1;
 
 class Animal {
+public:
+    Animal(const std::string &newspecies);
+
+    Animal(const std::string &species, Gender gender);
+
+    Animal(const std::string &species, Gender gender, float weight);
+
+    Animal(float weight);
+
+    Animal(const std::string &species, float weight);
+
+    static const std::string &getKingdom();
+
+    const std::string &getSpecies() const;
+
+    Gender getGender() const;
+
+    void setGender(Gender gender);
+
+    float getWeight() const;
+
+    void setWeight(float weight);
+
+    void printInfo();
+
+    bool isvalid();
+
 protected:
     static const std::string kingdom;
     std::string species;
     enum Gender gender = UNKNOWN_GENDER;
     float weight = UNKNOWN_WEIGHT;
 
-
 };
+
 
 
 #endif //EE205_LAB08C_ALL_CREATURES_GREAT_AND_SMALL_ANIMAL_H
